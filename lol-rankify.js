ef8756f795e35a038050e304511f5b4153e7ca28
Erin <erindazil@gmail.com>
2020-03-19 17:31:18 +0100

    + version for npm.js

--- lol-rankify.js
+++ lol-rankify.js
@@ -1,6 +1,6 @@
 registerPlugin({
     name: 'League Of Legends Rankify',
-    version: '1.4.0',
+    version: '1.4.1',
     backends: ['ts3'],
     description: 'Adds the corresponding League Of Legends Rank, Level, Role & InGame status for each user',
     author: 'Erin McGowan <sinusbot_lolrankify@protected.calmarsolutions.ch>',
